ad13324979ba04420b600ce11cb9b5346ba25204
Shreypatel46 <shreyp2046@gmail.com>
2024-02-20 13:54:47 +0530

    api context started

---
+++ 08minicontext/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+      <h1>chai aur react me context api sikhte he </h1>
+    </>
+  )
+}
+
+export default App
